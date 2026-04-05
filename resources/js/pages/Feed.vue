84a40451c5344eca882ec3453b9179a5d97b7aa2
Michael Blijleven <info@voicecode.nl>
2026-04-05 04:45:52 +0200

    Guard against undefined posts prop on Feed and Profile pages
    Adds v-if="posts" checks before accessing posts.data to prevent
    TypeError on iOS where component may render before props are available.

--- resources/js/pages/Feed.vue
+++ resources/js/pages/Feed.vue
@@ -61,11 +61,11 @@ const { t } = useTranslations();
             </Link>
         </div>
 
-        <InfiniteScroll data="posts" only-next>
+        <InfiniteScroll v-if="posts" data="posts" only-next>
             <PostCard v-for="post in posts.data" :key="post.id" :post="post" />
         </InfiniteScroll>
 
-        <div v-if="posts.data.length === 0" class="flex flex-col items-center justify-center px-8 py-20">
+        <div v-if="posts && posts.data.length === 0" class="flex flex-col items-center justify-center px-8 py-20">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="mb-4 size-16 text-sand-300 dark:text-sand-600">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Z" />
