dd27efa6028fe42a9a43fa4454bbd20adba5a1d4
Michael Blijleven <info@voicecode.nl>
2026-04-12 13:35:42 +0200

    Media selection improvements

--- resources/js/pages/CreatePost.vue
+++ resources/js/pages/CreatePost.vue
@@ -159,20 +159,15 @@ function submit() {
                 </div>
 
                 <!-- Upload Placeholder -->
-                <div v-else class="flex flex-col items-center justify-center px-8 py-16">
-                    <button
-                        class="flex flex-col items-center gap-3"
-                        @click="openSourcePicker"
-                    >
-                        <div class="flex size-20 items-center justify-center rounded-full bg-sand-100 dark:bg-sand-800">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="size-10 text-sand-400 dark:text-sand-500">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Z" />
-                            </svg>
-                        </div>
-                        <span class="text-sm font-medium text-sand-500 dark:text-sand-400">{{ t('Add a photo') }}</span>
-                    </button>
-                </div>
+                <button v-else class="flex w-full flex-col items-center justify-center px-8 py-16 active:bg-sand-50 dark:active:bg-sand-800" @click="openSourcePicker">
+                    <div class="flex size-20 items-center justify-center rounded-full bg-sand-100 dark:bg-sand-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="size-10 text-sand-400 dark:text-sand-500">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Z" />
+                        </svg>
+                    </div>
+                    <span class="mt-3 text-sm font-medium text-sand-500 dark:text-sand-400">{{ t('Add a photo') }}</span>
+                </button>
 
                 <p v-if="form.errors.media_path" class="px-4 pb-3 text-xs text-blush-500">{{ form.errors.media_path }}</p>
             </div>
